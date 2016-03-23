d9fab495ff841984fc3b2e57cb52076c0f4a6ed8
Tenji Tembo <tenji240@gmail.com>
2016-03-23 10:14:02 -0700

    switching the order of the radar chart fixed one weird toggle issue

--- js/v1/driver.js
+++ js/v1/driver.js
@@ -1,5 +1,71 @@
 $(document).ready(function(){
   var data = [{
+    "className": "example_b",
+    "normalize": true,
+    "scale_max": 20,
+    "axes" : [
+      {
+        "axis" : "axis_one",
+        "value": 9,
+        "abs_min" : 0,
+        "min" : 0,
+        "abs_max" : 10,
+        "max" : 10,
+        "step": 1,
+        "drag": true
+      },
+      {
+        "axis" : "axis_two",
+        "value": 8,
+        "abs_min" : 0,
+        "min" : 0,
+        "abs_max" : 10,
+        "max" : 10,
+        "step": 1,
+        "drag": true
+      },
+      {
+        "axis" : "axis_three",
+        "value": 8,
+        "abs_min" : 0,
+        "min" : 0,
+        "abs_max" : 10,
+        "max" : 10,
+        "step": 1,
+        "drag": true
+      },
+      {
+        "axis" : "axis_four",
+        "value": 11,
+        "abs_min" : 0,
+        "min" : 0,
+        "abs_max" : 10,
+        "max" : 10,
+        "step": 1,
+        "drag": true
+      },
+      {
+        "axis" : "axis_five",
+        "value": 15,
+        "abs_min" : 0,
+        "min" : 0,
+        "abs_max" : 10,
+        "max" : 10,
+        "step": 1,
+        "drag": true
+      },
+      {
+        "axis" : "axis_six",
+        "value": 10,
+        "abs_min" : 0,
+        "min" : 0,
+        "abs_max" : 10,
+        "max" : 10,
+        "step": 1,
+        "drag": true
+      }
+    ]
+  },{
     "className": "example_a",
     "normalize": true,
     "scale_max": 20,
@@ -65,77 +131,11 @@ $(document).ready(function(){
         "drag": true
       }
     ]
-  },{
-    "className": "example_b",
-    "normalize": true,
-    "scale_max": 20,
-    "axes" : [
-      {
-        "axis" : "axis_one",
-        "value": 7,
-        "abs_min" : 0,
-        "min" : 0,
-        "abs_max" : 10,
-        "max" : 10,
-        "step": 1,
-        "drag": true
-      },
-      {
-        "axis" : "axis_two",
-        "value": 5,
-        "abs_min" : 0,
-        "min" : 0,
-        "abs_max" : 10,
-        "max" : 10,
-        "step": 1,
-        "drag": true
-      },
-      {
-        "axis" : "axis_three",
-        "value": 6,
-        "abs_min" : 0,
-        "min" : 0,
-        "abs_max" : 10,
-        "max" : 10,
-        "step": 1,
-        "drag": true
-      },
-      {
-        "axis" : "axis_four",
-        "value": 3,
-        "abs_min" : 0,
-        "min" : 0,
-        "abs_max" : 10,
-        "max" : 10,
-        "step": 1,
-        "drag": true
-      },
-      {
-        "axis" : "axis_five",
-        "value": 11,
-        "abs_min" : 0,
-        "min" : 0,
-        "abs_max" : 10,
-        "max" : 10,
-        "step": 1,
-        "drag": true
-      },
-      {
-        "axis" : "axis_six",
-        "value": 8,
-        "abs_min" : 0,
-        "min" : 0,
-        "abs_max" : 10,
-        "max" : 10,
-        "step": 1,
-        "drag": true
-      }
-    ]
   }];
 
   var config = {
-    radius: 5, 
-    width: 400, 
+    radius: 5,
+    width: 400,
     height: 400,
     maxValue: 20
   };
@@ -146,5 +146,5 @@ $(document).ready(function(){
   /*var temp = new RadarChart('#temp', data, {radius: 5, width: 400, height: 400});
   temp.draw();
   console.log(temp.config);*/
-  
+
 });
