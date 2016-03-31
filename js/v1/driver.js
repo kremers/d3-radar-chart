bf5e1a04bb6fef365ade3a9373a4db35b3730dbe
Mike Ihbe <mikejihbe@gmail.com>
2016-03-31 02:17:13 -0700

    Refactor the radar charts to fit most of the new API proposal. Minimize the public APIs. We still need to add support for steps (basically rounding), axis labels, min/max boundary functions, getting data in/out, etc

--- js/v1/driver.js
+++ js/v1/driver.js
@@ -1,61 +1,7 @@
 $(document).ready(function(){
-  var data = {
-    "axes" : [
-      {
-        "axis" : "axis_one",
-        "value": 9,
-        "min" : 0,
-        "max" : 20,
-        "step": 1,
-      },
-      {
-        "axis" : "axis_two",
-        "value": 8,
-        "min" : 0,
-        "max" : 20,
-        "step": 1,
-      },
-      {
-        "axis" : "axis_three",
-        "value": 8,
-        "min" : 0,
-        "max" : 20,
-        "step": 1,
-      },
-      {
-        "axis" : "axis_four",
-        "value": 11,
-        "min" : 0,
-        "max" : 20,
-        "step": 1,
-      },
-      {
-        "axis" : "axis_five",
-        "value": 15,
-        "min" : 0,
-        "max" : 20,
-        "step": 1,
-      },
-      {
-        "axis" : "axis_six",
-        "value": 10,
-        "min" : 0,
-        "max" : 20,
-        "step": 1,
-      }
-    ]
-  };
 
-  var config = {
-    className: "example_b",
-    radius: 5,
-    width: 500,
-    height: 500,
-    maxValue: 20
-  };
-
-  var radar = new RadarChart("#chart", data, config);
-  radar.draw();
+  /*var radar = new RadarChart("#chart", data, config);
+  radar.draw();*/
 
   /*var temp = new RadarChart('#temp', data, {radius: 5, width: 400, height: 400});
   temp.draw();
@@ -70,10 +16,10 @@ $(document).ready(function(){
 
 /*
 var axis = new RadarChartAxis([
-  {metric: "cpu", min: 0, max: 100},
-  {metric: "ram", min: 0, max: 100},
-  {metric: "network_io", min: 0, max: 1000},
-  {metric: "disk_io", min: 0, max: 10000},
+  {metric: "cpu", min: 0, max: 100, step: 1},
+  {metric: "ram", min: 0, max: 100, step: 1},
+  {metric: "network_io", min: 0, max: 1000, step: 10},
+  {metric: "disk_io", min: 0, max: 10000, step: 100},
 ]);
 var minMetrics = [{metric: "cpu", value: 10}, {metric: "ram", value: 10}, {metric: "network_id", value: 10}, {metric: "disk_id", value: 10}];
 var minOptions = { className: "min-radar", ... }
@@ -94,20 +40,39 @@ setInterval(function() {
 minRadarChart.getData() // So we can persist the boundaries when necessary
 
 // The full monty
-var chartSert = new RadarChartSet({width: 500, height: 500})
-    .setAxis(axis)
+var chartSert = new RadarChartSet(axis, {width: 500, height: 500})
     .addRadarChart(minRadarChart)
     .addRadarChart(maxRadarChart)
     .addRadarChart(liveRadarChart)
 */
 
 
+var axis = new RadarChartAxis([
+  {metric: "cpu", min: 0, max: 100, step: 1},
+  {metric: "ram", min: 0, max: 100, step: 1},
+  {metric: "network_io", min: 0, max: 100, step: 1},
+  {metric: "disk_io", min: 0, max: 100, step: 1},
+  {metric: "blah1", min: 0, max: 100, step: 1},
+  {metric: "blah2", min: 0, max: 100, step: 1},
+], {
+  width: 500,
+  height: 500,
+  factorLegend: 1,
+  radians: 2 * Math.PI,
+  maxValue: 100, // TODO: Make this dynamic
+});
+var minMetrics = [{metric: "cpu", value: 10}, {metric: "ram", value: 10}, {metric: "network_io", value: 10}, {metric: "disk_io", value: 10}, {metric: "blah1", value: 10}, {metric: "blah2", value: 10}];
+var minOptions = { className: "min-radar" }
+var minRadarChart = new NewRadarChart(minMetrics, minOptions);
 
+/*var maxOptions = { className: "max-radar" }
+var maxMetrics = [{metric: "cpu", value: 80}, {metric: "ram", value: 80}, {metric: "network_io", value: 800}, {metric: "disk_io", value: 1000}, {metric: "blah1", value: 90}, {metric: "blah2", value: 100}]
+var maxRadarChart = new NewRadarChart(maxMetrics, maxOptions);
+*/
 
-
-
-
-
+var chartSet = new RadarChartSet("#chart", axis, {width: 500, height: 500});
+  chartSet.addRadarChart(minRadarChart)
+  //chartSet.addRadarChart(maxRadarChart)
 
 
 
--- js/v1/radar_v2.js
+++ js/v1/radar_v2.js
@@ -5,15 +5,334 @@
  * Prototypes and Public functions
  */
 
+
+/*
+var axis = new RadarChartAxis([
+  {metric: "cpu", min: 0, max: 100, step: 1},
+  {metric: "ram", min: 0, max: 100, step: 1},
+  {metric: "network_io", min: 0, max: 1000, step: 10},
+  {metric: "disk_io", min: 0, max: 10000, step: 100},
+  ...
+],
+{
+  width: 500,
+  height: 500,
+  factorLegend: 1,
+  radians: 2 * Math.PI,
+  maxValue: 20,
+}
+);
+*/
+function RadarChartAxis(paramArray, config) {
+  var self = this;
+  this.metricDetails = {};
+  this.draw = draw;
+  this.xCoord = xCoord;
+  this.yCoord = yCoord;
+  this.xCoordToValue = xCoordToValue;
+  this.yCoordToValue = yCoordToValue;
+  activate();
+
+  function activate() {
+    self.axes = paramArray;
+    self.config = _.extend({
+      factor: 1,
+      width: 500,
+      height: 500,
+      factorLegend: 1,
+      radians: 2 * Math.PI
+    }, config);
+    self.numberOfAxes = paramArray.length;
+    _.each(paramArray, function(param) {
+      self.metricDetails[param.metric] = param;
+    });
+  }
+
+  function nameToIndex(axisName) {
+    return _.findIndex(self.axes, function(axis) { return axis.metric == axisName; });
+  }
+
+  function xCoord(axisName, value) {
+    return this.config.width / 2.0 * (1 - (parseFloat(Math.max(value, 0)) / this.config.maxValue) * this.config.factor * Math.sin(nameToIndex(axisName) * this.config.radians / this.numberOfAxes));
+  }
+
+  function yCoord(axisName, value) {
+    return this.config.height / 2 * (1 - (parseFloat(Math.max(value, 0)) / this.config.maxValue) * this.config.factor * Math.cos(nameToIndex(axisName) * this.config.radians / this.numberOfAxes));
+  }
+
+  function xCoordToValue(axisName, xCoord) {
+    return this.config.maxValue * ( (1 - 2 * xCoord / this.config.width) / Math.sin(this.config.radians * nameToIndex(axisName) / this.numberOfAxes) / this.config.factor);
+  }
+
+  function yCoordToValue(axisName, yCoord) {
+    return this.config.maxValue * ( (1 - 2 * yCoord / this.config.height) / Math.cos(this.config.radians * nameToIndex(axisName) / this.numberOfAxes) / this.config.factor);
+  }
+
+
+  /*
+    Graph is a D3 SVG object
+
+  */
+  function draw(graph) {
+    var axis = graph.selectAll(".axis")
+                         .data(_.pluck(this.axes, "metric"))
+                         .enter()
+                         .append("g")
+                         .attr("class", "axis");
+
+    //add the corresponding lines for each unique axis
+    axis.append("line")
+        .attr("x1", self.config.width / 2.0)
+        .attr("y1", self.config.height / 2.0)
+        .attr("x2", function(axis, index){
+          return self.xCoord(axis, self.config.maxValue);
+        })
+        .attr("y2", function(axis, index){
+          return self.yCoord(axis, self.config.maxValue);
+        })
+        .attr("class", function(axis, index){
+          return "line-"+index;
+        })
+        .style("stroke", "#7f8c8d")
+        .style("stroke-width", "2px");
+
+
+    // TODO: make this actually work
+    //this will be used for the tooltips so that lables
+    //and legends can be added to the chart for better viewing
+    axis.append("text")
+        .text(function(axis){return axis;})
+        .style("font-family", "Helvetica")
+        .style("font-size", "10px")
+        .attr("transform", function (axis, index) { return "translate(0, -10)"; })
+        .attr("x", function(axis, index){
+          // TODO: Not sure what these do
+          return self.config.width / 2.0 * (1 - self.config.factorLegend * Math.sin(index * self.config.radians / self.numberOfAxes)) - 20 * Math.sin(index * self.config.radians / self.numberOfAxes);
+        })
+        .attr("y", function(axis, index){
+          return self.config.height / 2.0 * (1 - Math.cos(index * self.config.radians / self.numberOfAxes) + 20 * Math.cos(index * self.config.radians / self.numberOfAxes));
+        });
+  }
+}
+
+/*
+  id: the html id of this chartSert
+  axis: a RadarChartAxis object
+  options: an object that contains: width, height, factorLegend, radians, maxValue
+*/
+function RadarChartSet(id, axis, options) {
+  var self = this;
+  this.addRadarChart = addRadarChart;
+  activate();
+
+
+  function activate() {
+    self.id = id;
+    self.options = options;
+    self.axis = axis;
+
+    //get rid of any remaining svgs
+    d3.select(id).select("svg").remove();
+
+    //create the graph
+    self.graph = d3.select(id)
+                  .append("svg")
+                  .attr("width", axis.config.width)
+                  .attr("height", axis.config.height);
+    self.axis.draw(self.graph);
+  }
+
+  function addRadarChart(chart) {
+    chart.draw(this.axis, this.graph)
+  }
+}
+
+function NewRadarChart(data, options) {
+  var self = this;
+  this.setOptions = setOptions;
+  this.draw = draw;
+  activate();
+
+  function activate() {
+    self.data = data;
+    //this.options = options; // Not used, this should overwrite the config
+    self.config = _.extend({
+      className: "rename-me",
+      radius: 8, // Radius of a Point
+      width: 600,
+      height: 600,
+      factor: 1, // Scaling Box // TODO: remove this?
+      factorLegend: 0.85,
+      radians: 2 * Math.PI,
+      opacityArea: 0.5,
+      color: d3.scale.category10()[0],
+    }, options);
+  }
+
+  function setOptions(options) {
+    if(options) {
+      for(var i in options){
+        this.config[i] = options[i];
+      }
+    };
+  }
+
+  function draw(axis, graph) {
+    renderNodes(this.data, axis, graph);
+    update(axis, graph);
+  }
+
+  function renderNodes(data, axis, graph) {
+
+    // closure in renderNodes
+    function moveStep(dataPoint, index) {
+      var target = d3.select(this);
+
+      var segmentOrigin = {x: axis.config.width / 2, y: axis.config.width / 2}; // p0
+      var segmentEnd = {x: axis.xCoord(dataPoint.metric, axis.config.maxValue), y: axis.yCoord(dataPoint.metric, axis.config.maxValue)} // p1
+
+      // Stolen from: http://bl.ocks.org/mbostock/4281513
+      var x10 = segmentEnd.x - segmentOrigin.x,
+          y10 = segmentEnd.y - segmentOrigin.y,
+          x20 = d3.event.x - segmentOrigin.x,
+          y20 = d3.event.y - segmentOrigin.y,
+          pointLineSegmentParam = (x20 * x10 + y20 * y10) / (x10 * x10 + y10 * y10)
+          newXValue = segmentOrigin.x + pointLineSegmentParam * x10,
+          newYValue = segmentOrigin.y + pointLineSegmentParam * y10;
+
+      _.each(data, function(a) {
+        if(a.metric === dataPoint.metric) {
+          var valBasedOnX = axis.xCoordToValue(dataPoint.metric, newXValue),
+              valBasedOnY = axis.yCoordToValue(dataPoint.metric, newYValue),
+              newValue; // Final value
+          // Check for crazy values and axes that overlap with our coordinate system's before accepting values
+          if (!isNaN(valBasedOnX) && isFinite(valBasedOnX) && (newXValue > axis.config.width / 2 + 1 && newXValue < axis.config.width / 2 - 1)) {
+            newValue = valBasedOnX;
+          } else if (!isNaN(valBasedOnY) && isFinite(valBasedOnY)) {
+            newValue = valBasedOnY;
+          } else {
+            console.log(" NEW VAL explodes: " + valBasedOnX + " - " + valBasedOnY);
+          }
+          console.log({d3x: d3.event.x, d3y: d3.event.y, d3dx: d3.event.dx, d3dy: d3.event.dy, x: newXValue, y: newYValue, xValue: valBasedOnX, yValue: valBasedOnY})
+
+          //newValue = Math.max(newValue, 0);
+          //newValue = Math.min(newValue, a.max);
+          if (!(newValue < 0 || newValue > axis.metricDetails[dataPoint.metric].max)) {
+            a.value = newValue;
+
+            target.attr("cx", function(){ return newXValue; })
+                  .attr("cy", function(){ return newYValue; });
+          } else {
+            console.log("Won't extend axis beyond max value: " + axis.metricDetails[dataPoint.metric].max + ", have: " + newValue);
+          }
+        }
+      });
+
+      update(axis, graph);
+
+      graph.projection = d3.select('svg').append("path")
+          .attr("class", "line");
+      d3.select('.line').attr("d", "M" + [d3.event.x, d3.event.y] + "L" + [newXValue, newYValue]);
+    }; // End moveStep
+
+
+    graph.selectAll(".nodes")
+         .data(data)
+         .enter()
+         .append("svg:circle").attr("class", self.config.className)
+         .attr("r", self.config.radius)
+         .attr("alt", function(dataPoint){ return Math.max(dataPoint.value, 0); })
+         .attr("cx", function(dataPoint, index) {
+            return axis.xCoord(dataPoint.metric, dataPoint.value);
+         })
+         .attr("cy", function(dataPoint, index){
+            return axis.yCoord(dataPoint.metric, dataPoint.value)
+         })
+         .attr("data-id", function(dataPoint){ return dataPoint.metric; })
+         .attr("circle-class", self.config.className)
+         .style("fill", self.config.color)
+         .style("fill-opacity", 0.9)
+         .call(
+           d3.behavior.drag().on("drag", moveStep))
+         .append("svg:title")
+         .text(function (dataPoint) {
+           return Math.max(dataPoint.value, 0);
+         });
+  }
+
+  function update(axis, graph) {
+    //get rid of any remaining svgs
+    d3.select("polygon." + self.config.className).remove();
+
+    // generate polygon data
+    var dataValues = [];
+    graph.selectAll(".nodes")
+      .data(self.data, function(dataPoint, index){
+        dataValues[index] = [
+          axis.xCoord(dataPoint.metric, dataPoint.value),
+          axis.yCoord(dataPoint.metric, dataPoint.value)
+        ];
+      });
+    var dataPoints = {
+      className: self.config.className,
+      data : dataValues
+    };
+
+    //render polygon
+    var poly = generatePolygon(dataPoints, graph);
+    renderPolygon(poly);
+  }
+
+
+  //for a data points object (bounded by the datapoints class name)
+  //create a new polygon object
+  //returns the generated polygon
+  function generatePolygon(dataPoints, graph) {
+    var polygon = graph.selectAll("area")
+                       .data([dataPoints.data])
+                       .enter()
+                       .append("polygon")
+                       .attr("class", self.config.className)
+                       .style("stroke-width", "2px")
+                       .style("stroke", self.config.color)
+                       .on("mouseover", function(object){
+                          element = "polygon." + self.config.className;
+                          graph.selectAll("polygon").transition(200).style("fill-opacity", 0.1);
+                          graph.selectAll(element).transition(200).style("fill-opacity", 0.7);
+                       })
+                       .on("mouseout", function(){
+                           graph.selectAll("polygon").transition(200).style("fill-opacity", self.config.opacityArea);
+                       })
+                       .style("fill", function(data, i){ return self.config.color; })
+                       .style("fill-opacity", self.config.opacityArea);
+    return polygon;
+  };
+
+  //for a given polygon, return a string of points generated from recalulate points
+  //this is the actual function that renders the polygon on top
+  function renderPolygon(polygon) {
+    polygon.attr("points", function(data){
+      var string =  "";
+      for(var index = 0; index < data.length; index++){
+        string = string + data[index][0] + "," + data[index][1] + " ";
+      }
+      return string;
+    });
+  };
+}
+
+
+
+
+
+
 //Constructor
-function RadarChart(id, data, options){
+function RadarChart(id, data, options){ // done
 
   //Declarations
   this.id = id;
   this.data = data;
   //this.options = options; // Not used, this should overwrite the config
-  this.maxAxisValues = [];
-  this.minAxisValues = [];
   this.config = {
     className: "rename-me",
     radius: 8, // Radius Point
@@ -34,7 +353,7 @@ function RadarChart(id, data, options){
 
 //based on the options passed in by the user,
 //update the configuration
-RadarChart.prototype.updateConfiguration = function(options) {
+RadarChart.prototype.updateConfiguration = function(options) { // done
   if(options)
     for(var i in options){
       this.config[i] = options[i];
@@ -43,7 +362,7 @@ RadarChart.prototype.updateConfiguration = function(options) {
 
 //based on the data passed in
 //update the scale the radar chart will use
-RadarChart.prototype.updateScale = function() {
+RadarChart.prototype.updateScale = function() { // skip
   var max_array = [];
   var result =_.max(this.data.axes, function(axis){ return axis.value; });
   max_array.push(result.value);
@@ -53,7 +372,7 @@ RadarChart.prototype.updateScale = function() {
 
 // binds the arrays and the number of total axes
 //to the Radar chart object
-RadarChart.prototype.addAxisNames = function() {
+RadarChart.prototype.addAxisNames = function() { // skip
   var names = [];
   _.each(this.data.axes, function(axis){ names.push(axis.axis); });
 
@@ -64,34 +383,34 @@ RadarChart.prototype.addAxisNames = function() {
 };
 
 //binds the radar radius to the RadarChart
-RadarChart.prototype.computeRadius = function() {
+RadarChart.prototype.computeRadius = function() { // skip
   this.radarRadius = this.config.factor * Math.min(this.config.width / 2.0, this.config.height / 2.0);
 };
 
-RadarChart.prototype.drawFrame = function() {
+RadarChart.prototype.drawFrame = function() { // skip
   //to be added later for visual purposes of course
 };
 
-RadarChart.prototype.xCoord = function(axisIndex, value) {
+RadarChart.prototype.xCoord = function(axisIndex, value) { // done
   return this.config.width / 2.0 * (1 - (parseFloat(Math.max(value, 0)) / this.config.maxValue) * this.config.factor * Math.sin(axisIndex * this.config.radians / this.numberOfAxes));
 }
 
-RadarChart.prototype.yCoord = function(axisIndex, value) {
+RadarChart.prototype.yCoord = function(axisIndex, value) { // done
   return this.config.height / 2 * (1 - (parseFloat(Math.max(value, 0)) / this.config.maxValue) * this.config.factor * Math.cos(axisIndex * this.config.radians / this.numberOfAxes));
 }
 
-RadarChart.prototype.xCoordToValue = function(axisIndex, xCoord) {
+RadarChart.prototype.xCoordToValue = function(axisIndex, xCoord) { // done
   return this.config.maxValue * ( (1 - 2 * xCoord / this.config.width) / Math.sin(this.config.radians * axisIndex / this.numberOfAxes) / this.config.factor);
 }
 
-RadarChart.prototype.yCoordToValue = function(axisIndex, yCoord) {
+RadarChart.prototype.yCoordToValue = function(axisIndex, yCoord) { // done
   return this.config.maxValue * ( (1 - 2 * yCoord / this.config.height) / Math.cos(this.config.radians * axisIndex / this.numberOfAxes) / this.config.factor);
 }
 
 
 //this is designed to draw all unique axes that are passed in
 //even if the two radar charts have two different sets of data
-RadarChart.prototype.drawAxis = function() {
+RadarChart.prototype.drawAxis = function() { // done
   // body...
   //generate the axis element
   var radar = this; //bind this to RadarChart so that we can acess config objects and such
@@ -106,20 +425,10 @@ RadarChart.prototype.drawAxis = function() {
       .attr("x1", radar.config.width / 2.0)
       .attr("y1", radar.config.height / 2.0)
       .attr("x2", function(axis, index){
-        radar.maxAxisValues[index] = {
-          x: radar.xCoord(index, radar.config.maxValue),
-          y: 0
-        };
-        return radar.maxAxisValues[index].x;
+        return radar.xCoord(index, radar.config.maxValue);
       })
       .attr("y2", function(axis, index){
-        radar.maxAxisValues[index].y = radar.yCoord(index, radar.config.maxValue);
-        return radar.maxAxisValues[index].y;
-      })
-      .attr("line_slope", function(axis, index){
-        var dy = radar.maxAxisValues[index].y - (radar.config.height / 2.0);
-        var dx = radar.maxAxisValues[index].x - (radar.config.width / 2.0);
-        return dy/dx;
+        return radar.yCoord(index, radar.config.maxValue);
       })
       .attr("class", function(axis, index){
         return "line-"+index;
@@ -146,7 +455,7 @@ RadarChart.prototype.drawAxis = function() {
 
 //for a given radar chart, this calculates the data points required
 //returns a double array of the x & y coordinates
-RadarChart.prototype.calculatePoints = function(data) {
+RadarChart.prototype.calculatePoints = function(data) { // done
 
   var dataValues = [];
   var radar = this;
@@ -160,7 +469,7 @@ RadarChart.prototype.calculatePoints = function(data) {
   return dataValues;
 };
 
-RadarChart.prototype.baseStepScale = function () {
+RadarChart.prototype.baseStepScale = function () { // skip
   var baseScale = [];
   var radar = this;
   _.each(radar.data.axes, function(axis, index){
@@ -186,7 +495,7 @@ RadarChart.prototype.baseStepScale = function () {
 //for a data points object (bounded by the datapoints class name)
 //create a new polygon object
 //returns the generated polygon
-RadarChart.prototype.generatePolygon = function(dataPoints) {
+RadarChart.prototype.generatePolygon = function(dataPoints) { // done
   var radar = this;
   var polygon = radar.graph.selectAll("area")
                      .data([dataPoints.data])
@@ -210,7 +519,7 @@ RadarChart.prototype.generatePolygon = function(dataPoints) {
 
 //for a given polygon, return a string of points generated from recalulate points
 //this is the actual function that renders the polygon on top
-RadarChart.prototype.renderPolygon = function(polygon) {
+RadarChart.prototype.renderPolygon = function(polygon) { // done
   polygon.attr("points", function(data){
     var string =  "";
     for(var index = 0; index < data.length; index++){
@@ -220,7 +529,7 @@ RadarChart.prototype.renderPolygon = function(polygon) {
   });
 };
 
-RadarChart.prototype.renderNodes = function(data) {
+RadarChart.prototype.renderNodes = function(data) { // done
   var radar = this;
 
   function moveStep(axis, index) {
@@ -297,7 +606,7 @@ RadarChart.prototype.renderNodes = function(data) {
        });
 };
 
-RadarChart.prototype.update = function() {
+RadarChart.prototype.update = function() { // done
   var radar_chart = this;
   //get rid of any remaining svgs
   d3.select(radar_chart.id).select("polygon." + radar_chart.config.className).remove();
@@ -319,7 +628,7 @@ RadarChart.prototype.update = function() {
 //this function is the main driver of the application
 //given a radar chart and it's options, create & render
 //a multi-draggable radar chart
-RadarChart.prototype.draw = function() {
+RadarChart.prototype.draw = function() { // done
   var radar_chart = this;
   radar_chart.updateScale();
   radar_chart.addAxisNames();
